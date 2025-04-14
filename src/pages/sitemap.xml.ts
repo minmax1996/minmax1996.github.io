ee84ab7c025716cc2892debbfb4a9656153ea904
Maxim Minaev <minmax1996@mail.ru>
2025-04-15 00:50:12 +0300

    fix sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,8 +1,33 @@
-import { getCollection } from 'astro:content';
-import { SITE_URL } from '../consts';
+interface MarkdownFile {
+    frontmatter: {
+        draft?: boolean;
+        date: string;
+    };
+}
 
 export async function get() {
-    const posts = await getCollection('blog');
+    const baseUrl = 'https://www.minmax1996.me';
+
+    // Get all markdown files from the posts directory
+    const posts = await import.meta.glob<MarkdownFile>('./blog/posts/*.md');
+    console.log('Found posts:', Object.keys(posts));
+    
+    const publishedPosts = await Promise.all(
+        Object.entries(posts).map(async ([path, getPost]) => {
+            const post = await getPost();
+            if (post.frontmatter.draft) return null;
+            
+            const slug = path.split('/').pop()?.replace('.md', '');
+            console.log('Processing post:', slug, 'date:', post.frontmatter.date);
+            return {
+                slug,
+                date: post.frontmatter.date
+            };
+        })
+    ).then(posts => posts.filter(Boolean));
+
+    console.log('Published posts:', publishedPosts);
+
     const pages = [
         { url: '', changefreq: 'daily', priority: 1.0 },
         { url: 'blog', changefreq: 'daily', priority: 0.8 },
@@ -12,15 +37,15 @@ export async function get() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages.map(({ url, changefreq, priority }) => `
     <url>
-        <loc>${SITE_URL}/${url}</loc>
+        <loc>${baseUrl}/${url}</loc>
         <changefreq>${changefreq}</changefreq>
         <priority>${priority}</priority>
     </url>
     `).join('')}
-    ${posts.map(post => `
+    ${publishedPosts.map(post => `
     <url>
-        <loc>${SITE_URL}/blog/${post.id}</loc>
-        <lastmod>${post.data.date.toISOString()}</lastmod>
+        <loc>${baseUrl}/blog/posts/${post.slug}</loc>
+        <lastmod>${new Date(post.date).toISOString()}</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.7</priority>
     </url>
@@ -29,5 +54,8 @@ export async function get() {
 
     return {
         body: sitemap,
+        headers: {
+            'Content-Type': 'application/xml'
+        }
     };
 } 
